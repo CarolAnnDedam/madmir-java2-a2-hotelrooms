dcb87f72cff3aa27e72e80c674417f6bb3abca54
CarolAnnDedam <student@192.168.0.20>
2013-08-16 09:41:14 -0400

    finished HotelRooms

--- src/HotelBilling.java
+++ src/HotelBilling.java
@@ -1,21 +1,56 @@
+import javax.swing.JOptionPane;
+
 
 public class HotelBilling {
-
+	final static float SALES_TAX = .013f;
 	public static void main(String[] args) {
-		/*
-		 * In the main method, declare and initialize variables
-		 * for room price, number of rooms booked, and coupon rate.
-		 * 
-		 * Then call each case of the overloaded method 
-		 * calculateBill() to demonstrate the use of overloaded 
-		 * methods
-		 */
-	}
+		
+	
+	
+	 double roomRate = 95.00;
+	 double coupon = .010;
+	 int numberRooms = Integer.parseInt(JOptionPane.showInputDialog(" How many rooms would you like?"));
 
-	/*  
-	 * Here you will create four new methods:
-	 * -- Three cases of the overloaded method calculateBill()
-	 * -- A method called twoDecimalRounding() which will
-	 * 		be called by each case of the calculateBill() method.
-	 */
+	double totalBill= calculateBill(roomRate, coupon, numberRooms);
+	
+		 JOptionPane.showMessageDialog(null, "your total is $" + totalBill);
 }
+	 public static double calculateBill(double rate) {
+		 double tax = rate *SALES_TAX;
+		 double finalRate = rate + tax;
+		 finalRate = twoDecimalRounding(finalRate);
+		 return finalRate;
+		 
+	 }
+		 
+	public static double calculateBill(double rate, double coupon){
+		double tax = rate * SALES_TAX;
+		double finalRate = rate + tax;
+		finalRate = finalRate * (1 - coupon);
+		return finalRate;
+	}
+	public static double calculateBill(double rate, double coupon, int numberRooms){
+		double tax = rate * SALES_TAX;
+		double finalRate = rate + tax;
+		finalRate = finalRate * numberRooms * (1 - coupon);
+		finalRate = twoDecimalRounding(finalRate);
+		return finalRate;
+	}
+	
+	public static  double twoDecimalRounding(double decimal) {
+		decimal = decimal*100;
+		decimal = decimal + 0.5;
+		decimal =(int) decimal;
+		decimal = decimal/100;
+		return decimal;
+	}
+	
+	
+	
+	}
+	
+	
+	
+
+
+
